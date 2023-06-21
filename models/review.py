1e1497e2e7432abc26cc123f3043f3b9455c8b08
EnobongEdo <onlynenny@gmail.com>
2023-06-21 13:01:41 -0700

    updated

--- models/review.py
+++ models/review.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
 
-
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
