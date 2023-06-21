97b5bfabf72d380dfc751d73c84f597d5d34b929
EnobongEdo <onlynenny@gmail.com>
2023-06-21 13:06:32 -0700

    updated

--- models/amenity.py
+++ models/amenity.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
 
-
 class Amenity(BaseModel, Base):
     """Represents an amenity data set."""
     __tablename__ = 'amenities'
