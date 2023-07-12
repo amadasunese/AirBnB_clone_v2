23ebcaca74c31cd957419d21ad7baec187ecde60
Ese Amadasun <amadasunese@gmail.com>
2023-07-12 20:01:31 +0000

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,71 +1,38 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static.
-# Check if Nginx is installed
-if ! command -v nginx &>/dev/null; then
+# Sets up a web server for deployment of web_static.
 
-Install Nginx
 apt-get update
-apt-get install nginx -y
-fi
+apt-get install -y nginx
 
-# Create the /data/ directory if it doesn't exist
-if [ ! -d /data ]; then
-mkdir /data
-fi
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create the /data/web_static/ directory if it doesn't exist
-if [ ! -d /data/web_static ]; then
-mkdir /data/web_static
-fi
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create the /data/web_static/releases/ directory if it doesn't exist
-if [ ! -d /data/web_static/releases ]; then
-mkdir /data/web_static/releases
-fi
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Create the /data/web_static/shared/ directory if it doesn't exist
-if [ ! -d /data/web_static/shared ]; then
-mkdir /data/web_static/shared
-fi
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-# Create the /data/web_static/releases/test/ directory if it doesn't exist
-if [ ! -d /data/web_static/releases/test ]; then
-mkdir /data/web_static/releases/test
-fi
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
-# Create a fake HTML file /data/web_static/releases/test/index.html
-echo "This is a test file" > /data/web_static/releases/test/index.html
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
-if [ -L /data/web_static/current ]; then
-unlink /data/web_static/current
-fi
-
-ln -s /data/web_static/releases/test /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
-chown -R ubuntu:ubuntu /data
-
-# Update the Nginx configuration
-CWD=$(pwd)
-
-#Update the Nginx configuration
-
-echo "
-server {
-listen 80;
-server_name hbnb_static;
-
-location / {
-root $CWD/deployments/web_static/current;
-}
-}
-" > /etc/nginx/sites-available/hbnb_static
-
-# Enable the Nginx configuration
-ln -s /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/hbnb_static
-
-#Restart Nginx
 service nginx restart
-
-exit 0
