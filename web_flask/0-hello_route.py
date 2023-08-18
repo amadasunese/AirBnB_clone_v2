9f58e2df986205aeacc4e7478d57563026418327
Ese Amadasun <amadasunese@gmail.com>
2023-08-18 17:26:26 +0000

    updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,16 +2,16 @@
 '''a script that starts a Flask web application
 '''
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
